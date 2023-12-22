0364454850d517deea663e044354542c7f4303dd
xload1 <maratichts@gmail.com>
2023-12-22 18:14:18 +0100

    V 4.01 beta | Added more stuff for docker, unresolved issues remain

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM openjdk:19
-VOLUME /tmp
-EXPOSE 8080
-ARG JAR_FILE=out/artifacts/FeedbackCollectionSystem_jar/FeedbackCollectionSystem.jar
-ADD ${JAR_FILE} FeedbackCollectionSystem.jar
-ENTRYPOINT ["java","-jar","/FeedbackCollectionSystem.jar"]
--- src/main/java/com/example/fcs/FeedbackCollectionSystemApplication.java
+++ src/main/java/com/example/fcs/FeedbackCollectionSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FeedbackCollectionSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FeedbackCollectionSystemApplication.class, args);
+			SpringApplication.run(FeedbackCollectionSystemApplication.class, args);
 	}
 
 }
